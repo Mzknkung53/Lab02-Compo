f594ed0435d14fc05e5eea621eb987abcadefa4d
652115001 <Karanthaphong_aree@cmu.ac.th>
2024-07-24 20:32:26 +0700

    1.4

--- lab2/src/views/event/EventRegisterView.vue
+++ lab2/src/views/event/EventRegisterView.vue
@@ -1,26 +1,26 @@
 <script setup lang="ts">
-import type { Event } from '@/type';
-import EventService from '@/services/EventService';
-import { ref, onMounted, toRefs } from 'vue';
-import EventCard from '@/components/EventCard.vue'
-import { useRouter } from 'vue-router';
-import { useMessageStore } from '@/stores/message';
-import type { AxiosResponse } from 'axios';
+import type { Event } from '@/type'
+import { toRefs } from 'vue'
+import { useRouter } from 'vue-router'
+import { useMessageStore } from '@/stores/message'
 
 const props = defineProps<{
-    event: Event
+  event: Event
 }>()
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { event } = toRefs(props)
 const router = useRouter()
 const store = useMessageStore()
 const register = () => {
-    //
+  //
+  store.updateMessage('You are successfully registered for ' + props.event.title)
+  setTimeout(() => {
+    store.resetMessage()
+  }, 3000)
+  router.push({ name: 'event-detail-view', params: { id: props.event.id } })
 }
-
-
 </script>
 
 <template>
-    <button @click="register">Register</button>
+  <button @click="register">Register</button>
 </template>
