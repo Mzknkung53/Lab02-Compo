9a51355e82f1ea034e290561cfc38e5a1e1a6492
652115001 <Karanthaphong_aree@cmu.ac.th>
2024-07-24 22:46:01 +0700

    3.8

--- lab2/src/router/index.ts
+++ lab2/src/router/index.ts
@@ -9,10 +9,9 @@ import EventRegisterView from '@/views/event/EventRegisterView.vue'
 import EventLayoutView from '@/views/event/EventLayoutView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
 import NetworkErrorView from '@/views/NetworkErrorView.vue'
-import nProgress from'nprogress'
+import nProgress from 'nprogress'
 import EventService from '@/services/EventService'
-import {useEventStore} from '@/stores/event'
-
+import { useEventStore } from '@/stores/event'
 
 export function createAppRouter(pageLimit: (number | null)[]) {
   const router = createRouter({
@@ -38,63 +37,43 @@ export function createAppRouter(pageLimit: (number | null)[]) {
         component: StudentListView
       },
       {
-        path: '/404/:resource',
-        name: '404-resource',
-        component: NotFoundView,
-        props: true
-      },
-      {
-        path: '/:catchAll(.*)',
-        name: 'not-found',
-        component: NotFoundView
-      },
-      {
-        path: '/network-error',
-        name: 'network-error',
-        component: NetworkErrorView
-      },
-      
-      {
         path: '/event/:id',
         name: 'event-layout-view',
         component: EventLayoutView,
         props: true,
-        beforeEnter: (to) =>{
+        beforeEnter: (to) => {
           const id = parseInt(to.params.id as string)
           const eventStore = useEventStore()
-          return EventService.getEventById(id).then((response)=>{
-            eventStore.setEvent(response.data)
-
-          }).catch((error)=>{
-            if (error.response && error.response.status ===404){
-              return{
-                name: '404-resource-view',
-                params:{resource: 'event'}
+          return EventService.getEventById(id)
+            .then((response) => {
+              eventStore.setEvent(response.data)
+            })
+            .catch((error) => {
+              if (error.response && error.response.status === 404) {
+                return {
+                  name: '404-resource-view',
+                  params: { resourse: 'event' }
+                }
               }
-            } else{
-              return{name:'network-error-view'}
-            }
-          })
-
+            })
         },
         children: [
           {
             path: '',
             name: 'event-detail-view',
             component: EventDetailView,
-            props:true
+            props: true
           },
           {
             path: 'edit',
             name: 'event-edit-view',
-            props:true,
+            props: true,
             component: EventEditView
-            
           },
           {
             path: 'register',
             name: 'event-register-view',
-            props:true,
+            props: true,
             component: EventRegisterView
           }
         ]
@@ -110,13 +89,37 @@ export function createAppRouter(pageLimit: (number | null)[]) {
         name: 'event-register',
         props: true,
         component: EventRegisterView
+      },
+      {
+        path: '/404/:resource',
+        name: '404-resource',
+        component: NotFoundView,
+        props: true
+      },
+      {
+        path: '/:catchAll(.*)',
+        name: 'not-found',
+        component: NotFoundView
+      },
+      {
+        path: '/network-error',
+        name: 'network-error',
+        component: NetworkErrorView
       }
-    ]
+    ],
+   scrollBehavior(to,from,savedPosition){
+    if (savedPosition){
+      return savedPosition
+    } else{
+      return{top:0}
+    }
+
+    }
   })
-  router.beforeEach(() =>{
+  router.beforeEach(() => {
     nProgress.start()
   })
-  router.afterEach(()=>{
+  router.afterEach(() => {
     nProgress.done()
   })
   return router
