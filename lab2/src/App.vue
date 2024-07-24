63a637d05eb58db61bb815c8bc6ee99f500abea2
652115001 <Karanthaphong_aree@cmu.ac.th>
2024-07-24 21:14:21 +0700

    1.5

--- lab2/src/App.vue
+++ lab2/src/App.vue
@@ -1,16 +1,23 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { useMessageStore } from './stores/message'
+import { storeToRefs } from 'pinia';
 import EventCard from './components/EventCard.vue'
+const store = useMessageStore()
+const { message } = storeToRefs(store)
 </script>
 
 <template>
   <div id="layout">
     <header>
+      <div id="flashMessage" v-if="message">
+      <h4>{{ message }}</h4>
+    </div>
       <div class="wrapper">
         <nav>
-          <RouterLink :to="{name:'event-list-view'}">Event</RouterLink> |
-          <RouterLink :to="{name:'about'}">About</RouterLink>
-          <RouterLink :to="{name:'student'}">Student</RouterLink>
+          <RouterLink :to="{ name: 'event-list-view' }">Event</RouterLink> |
+          <RouterLink :to="{ name: 'about' }">About</RouterLink> |
+          <RouterLink :to="{ name: 'student' }">Student</RouterLink>
         </nav>
       </div>
     </header>
@@ -37,5 +44,18 @@ nav a.router-link-exact-active {
 
 h2 {
   font-size: 20px;
+}
+
+@keyframes yellofade {
+  from {
+    background-color: yellow;
+  }
+  to {
+    background-color: transparent;
+  }
+}
+
+#flashMessage {
+  animation: yellofade 3s ease-in-out
 }
 </style>
