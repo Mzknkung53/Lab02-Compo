25b5a6a3e3c46136c4350cd8e7900f8316b1a04d
652115001 <Karanthaphong_aree@cmu.ac.th>
2024-08-07 20:57:11 +0700

    4

--- lab2/src/components/EventCard.vue
+++ lab2/src/components/EventCard.vue
@@ -20,7 +20,7 @@ defineProps<{
 <template>
   <RouterLink class = "event-link" :to="{name: 'event-detail-view',params:{id: event?.id}}">
   <div class="event-class">
-    <div class="event-card">
+    <div class="event-card hover:scale-105 hover:shadow-sp">
       <h2>{{ event?.title }}</h2>
       <span>@{{ event?.time }} on {{ event?.date }}</span>
     </div>
@@ -36,10 +36,7 @@ defineProps<{
   border: 1px solid #39495c;
   margin-bottom: 18px;
 }
-.event-card:hover {
-  transform: scale(1.01);
-  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
-}
+
 .event-link{
   color: #2c3e50;
   text-decoration: none;
