0d4a9fe4f178a54b36a8f1cb29a9307d08da7c34
652115001 <Karanthaphong_aree@cmu.ac.th>
2024-08-07 21:07:33 +0700

    7

--- lab2/tailwind.config.js
+++ lab2/tailwind.config.js
@@ -2,15 +2,17 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}"
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       boxShadow: {
-        'sp': '0 3px 12px 0 rgba(0,0,0,0.2)',
-      }
+        'sp': '0 3px 12px 0 rgba(0,0,0, 0.2)'
+      },
+      fontFamily: {
+        sans: ['Avenir', 'Helvetica', 'Arial', 'sans-serif'],
+      },
     },
   },
   plugins: [],
 }
-
