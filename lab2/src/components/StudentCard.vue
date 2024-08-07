9e2c7e72dfa69137c10115c387d8c80fda37dbb9
652115001 <Karanthaphong_aree@cmu.ac.th>
2024-08-07 21:41:41 +0700

    11.3

--- lab2/src/components/StudentCard.vue
+++ lab2/src/components/StudentCard.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import type{Student} from '@/type'
+import type { Student } from '@/type'
 defineProps<{
   student: Student
 }>()
@@ -16,24 +15,14 @@ defineProps<{
 </script>
 
 <template>
-  <div class="student-class">
-    <div class="student-card">
-      <h2>{{ student.name + ' ' + student.surname+' '+student.gpa }}</h2>
-
+  <div class="flex flex-col items-center">
+    <div
+      class="p-5 w-[250px] cursor-pointer border border-[#39495c] mb-[18px] hover:scale-105 hover:shadow-lg transition-transform"
+    >
+      <h4>{{ student.id }} : {{ student.studentId }}</h4>
+      <h2>{{ student.name + ' ' + student.surname }}</h2>
+      <span>"{{ student.description }}"</span><br />
+      <span>gpa : {{ student.gpa }}</span>
     </div>
   </div>
 </template>
-
-<style scoped>
-.student-card {
-  padding: 20px;
-  width: 250px;
-  cursor: pointer;
-  border: 1px solid #39495c;
-  margin-bottom: 18px;
-}
-.student-card:hover {
-  transform: scale(1.01);
-  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
-}
-</style>
