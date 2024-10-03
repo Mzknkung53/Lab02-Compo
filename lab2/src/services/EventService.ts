0810a33d298321627000bcb46748f6bb4867f274
Mzknkung53 <chatchai_cha@cmu.ac.th>
2024-10-04 00:20:56 +0700

    Lab10 Q2

--- lab2/src/services/EventService.ts
+++ lab2/src/services/EventService.ts
@@ -11,15 +11,23 @@ const apiClient = axios.create({
 })
 
 export default {
+  // Fetch events with pagination
   getEvents(pageLimit: number, page: number): Promise<AxiosResponse<Event[]>> {
-
-    return apiClient.get<Event[]>('/events?_page='+page+'&_limit='+pageLimit)
+    return apiClient.get<Event[]>('/events?_page=' + page + '&_limit=' + pageLimit)
   },
 
+  // Fetch event by ID
   getEventById(id: number): Promise<AxiosResponse<Event>> {
     return apiClient.get<Event>(`/events/${id}`)
   },
-  saveEvent(event: Event){
+
+  // Save a new event
+  saveEvent(event: Event) {
     return apiClient.post('/events', event)
+  },
+
+  // Fetch events by keyword (title), with pagination
+  getEventsByKeyword(keyword: string, perPage: number, page: number): Promise<AxiosResponse<Event[]>> {
+    return apiClient.get<Event[]>('/events?title=' + keyword + '&_limit=' + perPage + '&_page=' + page)
   }
-  }
+}
